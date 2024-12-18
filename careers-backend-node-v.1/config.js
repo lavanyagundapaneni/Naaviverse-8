f1969b0e927c25d98b653a1a87dbc34b45d25c14
lavanya <137688652+lavanyagundapaneni@users.noreply.github.com>
2024-12-18 18:13:09 +0530

    Delete careers-backend-node-v.1 directory

--- careers-backend-node-v.1/config.js
+++
@@ -1,9 +0,0 @@
-const affiliate_system="affiliate_system"
-const gx_apps_profile="gx_apps_profile"
-const naavi_users="naavi_users"
-
-module.exports = {
-    affiliate_system,
-    gx_apps_profile,
-    naavi_users
-}
